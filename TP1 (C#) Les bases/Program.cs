c4bf2771aa9839ecb0a815244b0f4aaab663430f
Pel-One <abdelouahed.jamal@yahoo.com>
2026-01-21 16:25:48 +0100

    Ajoutez des fichiers projet.

---
+++ TP1 (C#) Les bases/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Salarié
+{
+    private int maricule;
+    private string nom;
+    private string prenom;
+    private double salaire;
+    private double tauxCS ;
+
+    public int Maricule { get => maricule; set => maricule = value; }
+    public string Nom { get => nom; set => nom = value; }
+    public string Prenom { get => prenom; set => prenom = value; }
+    public double Salaire { get => salaire; set => salaire = value; }
+    public double TauxCS { get => tauxCS; set => tauxCS = value; }
+    public Salarié(int maricule, string nom, string prenom, double salaire, double tauxCS)
+    {
+        Maricule = maricule;
+        Nom = nom;
+        Prenom = prenom;
+        Salaire = salaire;
+        TauxCS = tauxCS;
+    }
+}
